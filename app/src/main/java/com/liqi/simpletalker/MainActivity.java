162716a39c8d3d2310c393c85c4a7f0b3db7285e
qichanna <qichanna@126.com>
2017-06-14 12:37:23 +0800

    04-1-4

--- app/src/main/java/com/liqi/simpletalker/MainActivity.java
+++ app/src/main/java/com/liqi/simpletalker/MainActivity.java
@@ -1,14 +1,38 @@
 package com.liqi.simpletalker;
 
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.ViewTarget;
 import com.liqi.common.app.Activity;
+import com.liqi.common.widget.PortraitView;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 public class MainActivity extends Activity {
+
+    @BindView(R.id.appbar)
+    View mLayAppbar;
+
+    @BindView(R.id.im_portrait)
+    PortraitView mPortrait;
+
+    @BindView(R.id.txt_title)
+    TextView mTitle;
+
+    @BindView(R.id.lay_container)
+    FrameLayout mContainer;
+
+    @BindView(R.id.navigation)
+    BottomNavigationView mNavigation;
 
     @Override
     protected int getContentLayoutId() {
@@ -16,7 +40,29 @@ public class MainActivity extends Activity {
     }
 
     @Override
+    protected void initWidget() {
+        super.initWidget();
+
+        Glide.with(this).load(R.drawable.bg_src_morning).centerCrop().into(new ViewTarget<View,GlideDrawable>(mLayAppbar) {
+            @Override
+            public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
+                this.view.setBackground(resource.getCurrent());
+            }
+        });
+    }
+
+    @Override
     protected void initData() {
         super.initData();
+    }
+
+    @OnClick(R.id.im_search)
+    void onSearchMenuClick(){
+
+    }
+
+    @OnClick(R.id.btn_action)
+    void onActionClick(){
+
     }
 }
