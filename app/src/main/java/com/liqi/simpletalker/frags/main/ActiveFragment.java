91ca78c914d8583b680ba108b7da53b3d4b0c125
qichanna <qichanna@126.com>
2017-06-17 20:58:24 +0800

    04-2-2fragment加入

---
+++ app/src/main/java/com/liqi/simpletalker/frags/main/ActiveFragment.java
@@ -0,0 +1,25 @@
+package com.liqi.simpletalker.frags.main;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.liqi.common.app.Fragment;
+import com.liqi.simpletalker.R;
+
+
+public class ActiveFragment extends Fragment {
+
+
+    public ActiveFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    protected int getContentLayoutId() {
+        return R.layout.fragment_active;
+    }
+
+}
---
+++ app/src/main/java/com/liqi/simpletalker/frags/main/GroupFragment.java
@@ -0,0 +1,27 @@
+package com.liqi.simpletalker.frags.main;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.liqi.common.app.Fragment;
+import com.liqi.simpletalker.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GroupFragment extends Fragment {
+
+
+    public GroupFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    protected int getContentLayoutId() {
+        return R.layout.fragment_group;
+    }
+
+}
