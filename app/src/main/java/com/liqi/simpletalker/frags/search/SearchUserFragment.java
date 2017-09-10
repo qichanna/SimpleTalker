180059f910835cab2a44d0b09d0843c7e1fe2985
qichanna <qichanna@126.com>
2017-09-10 19:41:07 +0800

    7-10 07-3-2-客户端实现搜索人功能-2

--- app/src/main/java/com/liqi/simpletalker/frags/search/SearchUserFragment.java
+++ app/src/main/java/com/liqi/simpletalker/frags/search/SearchUserFragment.java
@@ -1,7 +1,17 @@
 package com.liqi.simpletalker.frags.search;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 import com.liqi.common.app.Fragment;
 import com.liqi.common.app.PresenterFragment;
+import com.liqi.common.widget.EmptyView;
+import com.liqi.common.widget.PortraitView;
+import com.liqi.common.widget.recycler.RecyclerAdapter;
 import com.liqi.simpletalker.R;
 import com.liqi.simpletalker.activities.SearchActivity;
 import com.liqi.talker.factory.model.UserCard.UserCard;
@@ -10,6 +20,8 @@ import com.liqi.talker.factory.presenter.search.SearchUserPresenter;
 
 import java.util.List;
 
+import butterknife.BindView;
+
 /**
  * 搜索群的界面实现
  */
@@ -17,14 +29,46 @@ public class SearchUserFragment extends PresenterFragment<SearchContract.Present
         implements SearchActivity.SearchFragment, SearchContract.UserView {
 
 
+    @BindView(R.id.empty)
+    EmptyView mEmptyView;
+
+    @BindView(R.id.recycler)
+    RecyclerView mRecycler;
+
+    private RecyclerAdapter<UserCard> mAdapter;
+
     public SearchUserFragment() {
-        // Required empty public constructor
+
     }
 
 
     @Override
     protected int getContentLayoutId() {
         return R.layout.fragment_search_user;
+    }
+
+    @Override
+    protected void initWidget(View root) {
+        super.initWidget(root);
+
+        // 初始化Recycler
+        mRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
+        mRecycler.setAdapter(mAdapter = new RecyclerAdapter<UserCard>() {
+            @Override
+            protected int getItemType(int position, UserCard userCard) {
+                // 返回cell的布局id
+                return R.layout.cell_search_list;
+            }
+
+            @Override
+            protected ViewHolder<UserCard> onCreateViewHolder(View root, int viewType) {
+                return new SearchUserFragment.ViewHolder(root);
+            }
+        });
+
+        // 初始化占位布局
+        mEmptyView.bind(mRecycler);
+        setPlaceHolderView(mEmptyView);
     }
 
     @Override
@@ -36,11 +80,43 @@ public class SearchUserFragment extends PresenterFragment<SearchContract.Present
     @Override
     public void onSearchDone(List<UserCard> userCards) {
         // 数据成功的情况下返回数据
+        mAdapter.replace(userCards);
+        // 如果有数据，则是OK，没有数据就显示空布局
+        mPlaceHolderView.triggerOkOrEmpty(mAdapter.getItemCount() > 0);
     }
 
     @Override
     protected SearchContract.Presenter initPresenter() {
         // 初始化presenter
         return new SearchUserPresenter(this);
+    }
+
+    /**
+     * 每一个cell的布局操作
+     */
+    class ViewHolder extends RecyclerAdapter.ViewHolder<UserCard> {
+        @BindView(R.id.im_portrait)
+        PortraitView mPortraitView;
+
+        @BindView(R.id.tet_name)
+        TextView mName;
+
+        @BindView(R.id.im_follow)
+        ImageView mFollow;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        protected void onBind(UserCard userCard) {
+            Glide.with(SearchUserFragment.this)
+                    .load(userCard.getPortrait())
+                    .centerCrop()
+                    .into(mPortraitView);
+
+            mName.setText(userCard.getName());
+            mFollow.setEnabled(userCard.isFollow());
+        }
     }
 }
