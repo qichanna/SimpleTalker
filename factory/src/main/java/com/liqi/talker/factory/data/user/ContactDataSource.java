b78dcb694556c77cfa13b9e15b5e8b6c11790af8
qichanna <qichanna@126.com>
2017-10-08 20:46:45 +0800

    8-13 08-3-4-客户端数据流封装-统一通知-1

---
+++ factory/src/main/java/com/liqi/talker/factory/data/user/ContactDataSource.java
@@ -0,0 +1,8 @@
+package com.liqi.talker.factory.data.user;
+
+/** 联系人数据源
+ * Created by liqi on 2017/10/8.
+ */
+
+public class ContactDataSource {
+}
---
+++ factory/src/main/java/com/liqi/talker/factory/data/user/ContactRepository.java
@@ -0,0 +1,8 @@
+package com.liqi.talker.factory.data.user;
+
+/** 联系人仓库
+ * Created by liqi on 2017/10/8.
+ */
+
+public class ContactRepository {
+}
