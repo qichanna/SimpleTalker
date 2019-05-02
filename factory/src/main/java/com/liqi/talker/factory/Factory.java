5eba37e0b12106bf492b026e303e1e46e93823ad
qichanna <qichanna@126.com>
2019-05-02 22:31:55 +0800

    9-1 9-2 9-3

--- factory/src/main/java/com/liqi/talker/factory/Factory.java
+++ factory/src/main/java/com/liqi/talker/factory/Factory.java
@@ -188,6 +188,7 @@ public class Factory {
         Log.e(TAG, model.toString());
         // 对推送集合进行遍历
         for (PushModel.Entity entity : model.getEntities()) {
+            Log.e(TAG, "dispatchPush-Entity: " + entity.toString() );
             switch (entity.type){
                 case PushModel.ENTITY_TYPE_LOGOUT:
                     instance.logout();
