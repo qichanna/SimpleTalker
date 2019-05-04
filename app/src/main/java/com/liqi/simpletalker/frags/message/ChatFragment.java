a611bacd18fa91348c9fc172ffef68458a2029c5
qichanna <qichanna@126.com>
2019-05-04 22:48:11 +0800

    9-10 --- 9-14
    9-10 9-11 9-12 9-13 9-14

--- app/src/main/java/com/liqi/simpletalker/frags/message/ChatFragment.java
+++ app/src/main/java/com/liqi/simpletalker/frags/message/ChatFragment.java
@@ -1,17 +1,119 @@
 package com.liqi.simpletalker.frags.message;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
 
 import com.liqi.common.app.Fragment;
+import com.liqi.common.widget.adapter.TextWatcherAdapter;
+import com.liqi.simpletalker.R;
 import com.liqi.simpletalker.activities.MessageActivity;
 
-public abstract class ChatFragment extends Fragment {
+import butterknife.BindView;
+import butterknife.OnClick;
 
-    private String mReceiverId;
+public abstract class ChatFragment extends Fragment
+        implements AppBarLayout.OnOffsetChangedListener {
+    protected String mReceiverId;
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @BindView(R.id.recycler)
+    RecyclerView mRecyclerView;
+
+    @BindView(R.id.appbar)
+    AppBarLayout mAppBarLayout;
+
+    @BindView(R.id.collapsingToolbarLayout)
+    CollapsingToolbarLayout mCollapsingLayout;
+
+    @BindView(R.id.edit_content)
+    EditText mContent;
+
+    @BindView(R.id.btn_submit)
+    View mSubmit;
 
     @Override
     protected void initArgs(Bundle bundle) {
         super.initArgs(bundle);
         mReceiverId = bundle.getString(MessageActivity.KEY_RECEIVER_ID);
+    }
+
+    @Override
+    protected void initWidget(View root) {
+        super.initWidget(root);
+
+        initToolbar();
+        initAppbar();
+        initEditContent();
+
+        // RecyclerView基本设置
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
+
+    // 初始化Toolbar
+    protected void initToolbar() {
+        Toolbar toolbar = mToolbar;
+        toolbar.setNavigationIcon(R.drawable.ic_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().finish();
+            }
+        });
+    }
+
+    //  给界面的Appbar设置一个监听，得到关闭与打开的时候的进度
+    private void initAppbar() {
+        mAppBarLayout.addOnOffsetChangedListener(this);
+    }
+
+    // 初始化输入框监听
+    private void initEditContent() {
+        mContent.addTextChangedListener(new TextWatcherAdapter() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                String content = s.toString().trim();
+                boolean needSendMsg = !TextUtils.isEmpty(content);
+                // 设置状态，改变对应的Icon
+                mSubmit.setActivated(needSendMsg);
+            }
+        });
+    }
+
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+    }
+
+    @OnClick(R.id.btn_face)
+    void onFaceClick() {
+        // TODO
+    }
+
+    @OnClick(R.id.btn_record)
+    void onRecordClick() {
+        // TODO
+    }
+
+    @OnClick(R.id.btn_submit)
+    void onSubmitClick() {
+        if (mSubmit.isActivated()) {
+            // 发送
+
+        } else {
+            onMoreClick();
+        }
+    }
+
+    private void onMoreClick() {
+        // TODO
     }
 }
--- app/src/main/java/com/liqi/simpletalker/frags/message/ChatUserFragment.java
+++ app/src/main/java/com/liqi/simpletalker/frags/message/ChatUserFragment.java
@@ -2,14 +2,27 @@ package com.liqi.simpletalker.frags.message;
 
 
 import android.app.Fragment;
+import android.support.design.widget.AppBarLayout;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
+import com.liqi.common.widget.PortraitView;
 import com.liqi.simpletalker.R;
+import com.liqi.simpletalker.activities.PersonalActivity;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ChatUserFragment extends ChatFragment {
 
+    @BindView(R.id.im_portrait)
+    PortraitView mPortrait;
+
+    private MenuItem mUserInfoMenuItem;
 
     public ChatUserFragment() {
         // Required empty public constructor
@@ -18,6 +31,81 @@ public class ChatUserFragment extends ChatFragment {
     @Override
     protected int getContentLayoutId() {
         return R.layout.fragment_chat_user;
+    }
+
+    @Override
+    protected void initToolbar() {
+        super.initToolbar();
+
+        Toolbar toolbar = mToolbar;
+        toolbar.inflateMenu(R.menu.chat_user);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.action_person) {
+                    onPortraitClick();
+                }
+                return false;
+            }
+        });
+
+        // 拿到菜单Icon
+        mUserInfoMenuItem = toolbar.getMenu().findItem(R.id.action_person);
+    }
+
+    // 进行高度的综合运算，透明我们的头像和Icon
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+        super.onOffsetChanged(appBarLayout, verticalOffset);
+        View view = mPortrait;
+        MenuItem menuItem = mUserInfoMenuItem;
+
+        if (view == null || menuItem == null)
+            return;
+
+
+        if (verticalOffset == 0) {
+            // 完全展开
+            view.setVisibility(View.VISIBLE);
+            view.setScaleX(1);
+            view.setScaleY(1);
+            view.setAlpha(1);
+
+            // 隐藏菜单
+            menuItem.setVisible(false);
+            menuItem.getIcon().setAlpha(0);
+        } else {
+            // abs 运算
+            verticalOffset = Math.abs(verticalOffset);
+            final int totalScrollRange = appBarLayout.getTotalScrollRange();
+            if (verticalOffset >= totalScrollRange) {
+                // 关闭状态
+                view.setVisibility(View.INVISIBLE);
+                view.setScaleX(0);
+                view.setScaleY(0);
+                view.setAlpha(0);
+
+                // 显示菜单
+                menuItem.setVisible(true);
+                menuItem.getIcon().setAlpha(255);
+
+            } else {
+                // 中间状态
+                float progress = 1 - verticalOffset / (float) totalScrollRange;
+                view.setVisibility(View.VISIBLE);
+                view.setScaleX(progress);
+                view.setScaleY(progress);
+                view.setAlpha(progress);
+                // 和头像恰好相反
+                menuItem.setVisible(true);
+                menuItem.getIcon().setAlpha(255 - (int) (255 * progress));
+            }
+        }
+    }
+
+    @OnClick(R.id.im_portrait)
+    void onPortraitClick() {
+        PersonalActivity.show(getContext(), mReceiverId);
     }
 
 }
